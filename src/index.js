1471fae477ae1e5c2d2ce2272c3c2fe5077e0bfa
キンジョウ <73173075+AFURO-1130@users.noreply.github.com>
2022-05-11 21:29:06 +0900

    変更した hoge

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ console.log("hoge");
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  金城
 </div>
 `;
